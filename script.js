259d21e87711214ea056c553eba17e18903feb22
kielsouza <kielsouza115@hotmail.com>
2022-08-12 11:31:26 -0300

    Requisito 21 completo

--- script.js
+++ script.js
@@ -32,3 +32,46 @@ function habilitarBtnSend() {
 if (false) {
   habilitarBtnSend();
 }
+
+function validarForm() {
+  var formNome = document.getElementById('input-name').value;
+  var formSobrenome = document.getElementById('input-lastname').value;
+  var formEmail = document.getElementById('input-email').value;
+  var formCasa = document.getElementById('house').options[document.getElementById('house').selectedIndex].value;
+  var arrayFamilia = document.getElementsByName('family');
+  var formFamilia;
+  var arrayConteudo = document.getElementsByName('conteudo');
+  var formConteudo = [];
+  var arrayAvalia = document.getElementsByName('rate');
+  var formAvalia;
+  var formText = document.getElementById('textarea').value;
+
+  for (let i=0; i<arrayFamilia.length; i++) {
+    if(arrayFamilia[i].checked) {
+      formFamilia = arrayFamilia[i].value;
+    }
+  }
+
+  for (let i=0; i<arrayConteudo.length; i++) {
+    if(arrayConteudo[i].checked) {
+      formConteudo.push(arrayConteudo[i].value);
+    }
+  }
+
+  for (let i=0; i<arrayAvalia.length; i++) {
+    if(arrayAvalia[i].checked) {
+      formAvalia = arrayAvalia[i].value;
+    }
+  }
+
+  document.getElementById('evaluation-form').style.display = "none";
+  document.getElementById('form-data').style.display = "block";
+  document.getElementById('name-data').innerHTML = formNome + " " +formSobrenome;
+  document.getElementById('email-data').innerHTML = formEmail;
+  document.getElementById('house-data').innerHTML = formCasa;
+  document.getElementById('family-data').innerHTML = formFamilia;
+  document.getElementById('conteudo-data').innerHTML = formConteudo.join(', ');
+  document.getElementById('rate-data').innerHTML = formAvalia;
+  document.getElementById('text-data').innerHTML = formText;
+ 
+}
